640386a86b919a3f2463317df2b16996ee4e6e68
carward25 <83989145+carward25@users.noreply.github.com>
2021-10-19 20:45:42 -0700

    Update fall_distance.py

--- fall_distance.py
+++ fall_distance.py
@@ -1,3 +1,7 @@
+#name: Cassidy Ward
+#date: 10/19/2021
+#description: the program takes time in seconds as an argument
+
 def fall_distance(falling_time):
     return 0.5 * 9.8 * falling_time * falling_time
 
