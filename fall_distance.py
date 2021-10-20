2122be5cefae02c71e6053933afb163c0bb98171
carward25 <83989145+carward25@users.noreply.github.com>
2021-10-19 20:46:22 -0700

    Update fall_distance.py

--- fall_distance.py
+++ fall_distance.py
@@ -6,5 +6,5 @@ def fall_distance(falling_time):
     return 0.5 * 9.8 * falling_time * falling_time
 
 
-time = float(input("Enter the amount of time for which the object is falling: "))
-print("Distance the object has fallen is", fall_distance(time))
+#time = float(input("Enter the amount of time for which the object is falling: "))
+#print("Distance the object has fallen is", fall_distance(time))
